8866bb0364f7b6b6e75bfa980fdc8e2463c7937b
Marek <119450312+MareksCode@users.noreply.github.com>
2025-12-05 10:33:32 +0100

    init

---
+++ Day5/part1.py
@@ -0,0 +1,10 @@
+import os 
+path = os.getcwd() + "\\Day5\\testSourceIDs.txt"
+f1 = open(path, "r")
+
+f1.close()
+
+path2 = os.getcwd() + "\\Day5\\testSourceRanges.txt"
+f2 = open(path2, "r")
+
+f2.close()
